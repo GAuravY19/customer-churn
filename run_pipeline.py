d9ed13cb08aeb713e6dc61356ef56eb18a348fc0
gaurav <gauravy1905@gmail.com>
2024-06-06 09:00:25 +0530

    full pipeline created

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,14 @@
 from pipeline.dataclean import DataCleanPipeline
-
 Clean = DataCleanPipeline()
 Clean.RunSteps()
 Clean.saveData()
+
+
+from pipeline.datascaling import DataScalePipeline
+Scale = DataScalePipeline()
+Scale.RunPipeline()
+Scale.savedata()
+
+from pipeline.Modelpipeline import ModelBuildPipeline
+Model = ModelBuildPipeline()
+Model.RunSteps()
